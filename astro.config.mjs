f2daace5f7f5ca7618552bfbfc7a6abfb232f234
Ssejjemba Dan <ssejjembadan@gmail.com>
2025-08-03 01:36:20 +0300

    Switch to Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,7 @@
-// @ts-check
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless"; // Use /edge for edge runtime
 
-import node from "@astrojs/node";
-
-// https://astro.build/config
 export default defineConfig({
   output: "server",
-
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
