d4c27901b8d3c4b1834538b45d4eee292220069a
Ssejjemba Dan <ssejjembadan@gmail.com>
2025-08-20 21:37:07 +0300

    feat: Add sitemap integration to Astro configuration for improved SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel"; // Use /edge for edge runtime
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://veteransdisabilitynetwork.org",
   output: "server",
   adapter: vercel(),
+  integrations: [sitemap()],
 });
